486b1e2196ff1c27434247cb29dde9ab3d574ea1
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-05-06 14:38:16 +1000

    task 9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last = number % 10
-    print("{}".format(("").join(last)))
+    if number < 0:
+        number = number * -1
+    last = str(number % 10)
+    print("{}".format(last), end="")
+    return last
