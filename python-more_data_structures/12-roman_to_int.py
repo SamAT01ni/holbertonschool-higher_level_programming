9b439a99f5ef3e64e65979ae4f13b77eb4507043
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-05-13 16:36:45 +1000

    task 12 roman numerals

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    val = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    }
+    sum = 0
+    i = 0
+    while i < len(roman_string):
+        currentval = val[roman_string[i]]
+        if (i+1 < len(roman_string) and
+            val[roman_string[i]] < val[roman_string[i + 1]]):
+            sum = sum + val[roman_string[i + 1]] - currentval
+            i += 2
+        else:
+            sum += currentval
+            i += 1
+    return sum
