c69883751c3bd8e114d6059256cb77fe74335df8
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-05-13 14:51:23 +1000

    no None

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    max_key = max(a_dictionary, key=a_dictionary.get)
-    return max_key if a_dictionary else None
+    if a_dictionary == None:
+        return None
+    else:
+        max_key = max(a_dictionary, key=a_dictionary.get)
+        return max_key
