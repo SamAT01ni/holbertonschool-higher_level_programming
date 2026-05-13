89004eecf673d5b28b0fda8330ee1ccdb86ad0c1
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-05-13 16:39:34 +1000

    not string define

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
+    if type(roman_string) is not str:
         return 0
     val = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
@@ -10,7 +10,7 @@ def roman_to_int(roman_string):
     while i < len(roman_string):
         currentval = val[roman_string[i]]
         if (i+1 < len(roman_string) and
-            val[roman_string[i]] < val[roman_string[i + 1]]):
+                val[roman_string[i]] < val[roman_string[i + 1]]):
             sum = sum + val[roman_string[i + 1]] - currentval
             i += 2
         else:
