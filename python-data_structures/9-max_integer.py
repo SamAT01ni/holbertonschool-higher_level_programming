987764a84df21951aa6827e2dc6583cafadceb2c
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-05-13 11:53:34 +1000

    empty case

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    high = my_list[0]
     if len(my_list) <= 0:
         return None
     else:
+        high = my_list[0]
         for i in range(len(my_list)):
             if my_list[i] > high:
                 high = my_list[i]
