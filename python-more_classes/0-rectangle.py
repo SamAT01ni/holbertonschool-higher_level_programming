ff89efedf19ddf040017a7bc5a338a545c3d51b7
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-05-20 13:35:15 +1000

    task 0 rectangle start

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Rectangles are my friend
+I changed the colours on my terminal and its a bad decision
+"""
+
+
+class Rectangle:
+    """ thats right, it goes in the *square* hole"""
+    pass
