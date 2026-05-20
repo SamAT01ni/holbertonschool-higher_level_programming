3c66f07b4389025cf580de67d98ab2359e871cf0
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-05-20 10:17:44 +1000

    task 1 size square

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+I am in Holberton today discussing the topic of squares
+Squares a 4 sided quadrilateral with equal sides and 90 degree angles
+"""
+
+
+class Square:
+    """It goes in the square hole"""
+    def __init__(self, size):
+        self.__size = size
