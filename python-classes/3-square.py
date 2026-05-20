67b7c7047e9a4c9260b96957a444804899b1012f
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-05-20 10:31:53 +1000

    task 3 area

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+I am in Holberton today discussing the topic of squares
+Squares a 4 sided quadrilateral with equal sides and 90 degree angles
+"""
+
+
+class Square:
+    """It goes in the square hole"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
