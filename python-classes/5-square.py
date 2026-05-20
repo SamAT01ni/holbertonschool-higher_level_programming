5f1069546f56c7c05e6c36f14a546b0ffe9744c0
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-05-20 10:45:45 +1000

    task 5 printy squary

---
+++ python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+I am in Holberton today discussing the topic of squares
+Squares a 4 sided quadrilateral with equal sides and 90 degree angles
+"""
+
+
+class Square:
+    """It goes in the square hole"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
