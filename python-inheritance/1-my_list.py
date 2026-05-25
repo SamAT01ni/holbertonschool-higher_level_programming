faa3b8084e5f533b9a4ced22a76155c1d0a83c78
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-05-25 15:55:10 +1000

    document the class

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """
 Module for using list sorting without affecting the original list
+Nothing ever happens
 """
 
 
 class MyList(list):
+    """ MyList module is my list.
+        And noone
+        Can take it away"""
     def print_sorted(self):
         sorted_list = sorted(self)
         print(sorted_list)
