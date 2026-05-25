020454463ddb253866e69dd258c296fb7aa15304
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-05-25 16:41:47 +1000

    task 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+kind of class module
+yknow whats kind of class?
+guinness
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """returns true or false i guess"""
+    return isinstance(obj, a_class)
