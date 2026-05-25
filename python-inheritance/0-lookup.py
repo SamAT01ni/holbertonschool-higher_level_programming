5955a6fea049141b27d597293579e3cd1b231a55
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-05-25 12:43:48 +1000

    do i really need to document 2 lines?

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""
+Module is able to look up attributes for the module
+
+I dont like it
+"""
+
+
 def lookup(obj):
+    """Return directory of the attributes"""
     return dir(obj)
