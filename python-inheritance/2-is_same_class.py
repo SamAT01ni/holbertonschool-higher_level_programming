e6cd1e304c7f4e4da90bd300a91855a226d951f5
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-05-25 16:31:36 +1000

    task 2 true or false

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+"same class" module
+Is a dream a lie if it doesnt come true
+or is it something worse
+"""
+
+
+def is_same_class(obj, a_class):
+    return type(obj) is a_class
