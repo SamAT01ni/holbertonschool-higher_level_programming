2ee7e2299947f031af9eb101b82503d6ae8946cc
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-05-26 16:31:15 +1000

    task 8 rectangle start

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+"""
+This is a recctangle module
+Getting deja vu yet?
+"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangles are 4 sided quadrilaterals with equal angles
+        and 2 sets of equal sides
+        Fascinating
+        """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
