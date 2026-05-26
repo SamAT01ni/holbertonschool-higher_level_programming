76232684729a4b1545643ca362c71e2adb9a9bde
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-05-26 15:17:26 +1000

    task 6 area exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Base Geometry module
+Who knows what we'll get up to here
+"""
+
+
+class BaseGeometry:
+    """ Base geometry
+        i gotta have this or betty will neuter me
+        """
+    def area(self):
+        raise Exception("area() is not implemented")
