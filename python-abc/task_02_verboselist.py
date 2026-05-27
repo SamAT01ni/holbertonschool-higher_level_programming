0b28cbc11e59a366f271da0fcd3328ed58efcb14
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-05-27 13:08:56 +1000

    please checker i cant take this anymore

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Verbose list
+    What the hell is verbose
+    """
+
+
+class VerboseList(list):
+    """ Inherits built in list with all them things
+        """
+    def append(self, item):
+        super().append(item)
+        print("Added [{}] to the list".format(item))
+
+
+    def extend(self, x):
+        super().extend(x)
+        print("Extended the list with [{}] items".format(len(x)))
+
+
+    def remove(self, item):
+        super().remove(item)
+        print("Removed [{}] from the list".format(item))
+
+
+    def pop(self, index=-1):
+        item = super().pop(index)
+        print("Popped [{}] from the list".format(item))
