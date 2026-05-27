fb9166312d42a857132da32658f97eb3ae247d84
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-05-27 13:12:18 +1000

    this checker will kill me/

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -9,16 +9,16 @@ class VerboseList(list):
         """
     def append(self, item):
         super().append(item)
-        print("Added [{}] to the list".format(item))
+        print("Added [{}] to the list.".format(item))
 
     def extend(self, x):
         super().extend(x)
-        print("Extended the list with [{}] items".format(len(x)))
+        print("Extended the list with [{}] items.".format(len(x)))
 
     def remove(self, item):
         super().remove(item)
-        print("Removed [{}] from the list".format(item))
+        print("Removed [{}] from the list.".format(item))
 
     def pop(self, index=-1):
         item = super().pop(index)
-        print("Popped [{}] from the list".format(item))
+        print("Popped [{}] from the list.".format(item))
