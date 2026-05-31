299c44d37d0373a286f3379dda95ffb37b83f322
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-05-31 14:55:42 +1000

    task 5 mixins

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" DRAGON
+the welsh like dragons, i am not so keen
+"""
+
+
+class SwimMixin:
+    def swim(self):
+        print("The creature swims!")
+
+
+class FlyMixin:
+    def fly(self):
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    def roar(self):
+        print("The dragon roars!")
