f4bd209748ccf4efa456c825c25362b74e15dfcd
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-06-03 15:33:27 +1000

    apparantly they werent happy with my l list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,12 +7,12 @@ import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 f = "add_item.json"
-l = []
+list1 = []
 
 try:
-    l = load_from_json_file(f)
+    list1 = load_from_json_file(f)
 except FileNotFoundError:
-    l = []
+    list1 = []
 
-l.extend(sys.argv[1:])
-save_to_json_file(l, f)
+list1.extend(sys.argv[1:])
+save_to_json_file(list1, f)
