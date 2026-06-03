ecbc6d95f6840a63fea45ea56e5172ff843e8d1f
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-06-03 14:31:13 +1000

    spelling error

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -8,4 +8,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """ i have a friend called jason which is close to json """
     with open(filename, "w") as f:
-        json.dump(my_obj, file)
+        json.dump(my_obj, f)
