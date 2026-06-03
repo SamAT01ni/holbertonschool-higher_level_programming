dcf6b9a8f5024707ed605a9439e3aed5c59009c8
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-06-03 13:56:09 +1000

    task 0 documented

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""" Module for reading a file using utf8
+what else
+"""
 
 def read_file(filename=""):
     """ uses with, opens files """
-    with open(filename, encoding="utf-9") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
