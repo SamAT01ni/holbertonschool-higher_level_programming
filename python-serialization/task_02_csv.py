cfdbb40567bfb61f3e45fca04ff848db8bebcb8e
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-06-07 19:44:00 +1000

    wallahi i will kms

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -7,11 +7,14 @@ import json
 
 
 def convert_csv_to_json(file):
+    """THrow shit at a wall and see what sticks"""
     try:
-        with open(file, mode="w", encoding="utf-8") as cf:
+        with open(file, mode="r", encoding="utf-8") as cf:
             data = list(csv.DictReader(cf))
+
         with open("data.json", mode="w", encoding="utf-8") as jf:
             json.dump(data, jf)
+
         return True
 
     except FileNotFoundError:
