fc15bd32eb931dd364ca1b301f0ab84950005fcf
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-06-07 20:06:04 +1000

    pascal you little freak

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Pascals triangle module"""
+
+
+def pascal_triangle(n):
+    """pascal was a weird dude"""
+    if n <= 0:
+        return []
+
+    pascal = [[height for height in range(n) if height < width + 1]
+              for width in range(n)]
+    for i in range(len(pascal)):
+        for j in range(len(pascal[i])):
+            val = 1
+            if j < i and i > 0 and j > 0:
+                val = pascal[i - 1][j] + pascal[i - 1][j - 1]
+            pt[i][j] = val
+    return pascal
