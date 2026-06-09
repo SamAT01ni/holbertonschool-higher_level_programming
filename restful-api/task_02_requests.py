07fea35057f6bf8d7cf7c89f2d3ee30bf575d161
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-06-09 20:45:01 +1000

    task 2 still needs fix

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import requests
+import csv
+import json
+
+
+def fetch_and_print_posts():
+    r = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print(f"Status Code: {r.status_code}")
+    if r.status_code == 200:
+        jsondata = r.json()
+        for post in jsondata:
+            print(post)
+
+
+def fetch_and_save_posts():
+    r = requests.get("https://jsonplaceholder.typicode.com/posts")
+    if r.status_code == 200:
+        jsondata = r.json()
+        data = [
+            {
+                "id": post["id"], "title": post["title"],
+                "body": post["body"]
+            } for post in jsondata]
+    with open("posts.csv", "w", encoding="utf-8") as f:
+        keys = ["id", "title", "body"]
+        writer = csv.DictWriter(f, fieldnames=keys)
+        writer.writeheader()
+        writer.writerows(data)
