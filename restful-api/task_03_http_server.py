f1862085caabb7ab36802dc22653c7bfa3a805e3
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-06-12 14:55:44 +1000

    task 3 wow wee thats rough

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import http.server
+import json
+
+
+PORT = 8000
+handler = http.server.BaseHTTPRequestHandler
+
+
+class Handler(handler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-Type", 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content_type", "application/json")
+            self.end_headers()
+            dataset = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(dataset).encode("utf-8"))
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            slef.wfile.write(b"OK")
+        else:
+            self.send_response(404)
+            self.send_header("Content-Type", 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"WHAT THE HELL ARE YOU DOING")
+
+
+if __name__ == "__main__":
+    server_address = ('', 8000)
+    httpd = http.server.HTTPServer(server_address, Handler)
+    print("Server running")
+    httpd.serve_forever()
