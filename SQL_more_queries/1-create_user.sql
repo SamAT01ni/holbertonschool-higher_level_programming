006556bc191121577af5b9220d314f345d6d721a
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-06-16 01:17:21 +0000

    try this, remove options

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Task 1, create user with all priveleges and password
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- give all privileges
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
