6c7a7573cfa48282ca8d772efd7ac5c018c6bbdd
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-06-17 03:25:27 +0000

    task 7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Task 7 cities table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- make the table
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES states(id)
+);
