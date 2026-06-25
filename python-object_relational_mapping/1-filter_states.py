f94510884cc005c75e07bed3c9b41a66c5b29fd3
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-06-25 02:49:07 +0000

    task 1 only starting with N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Listing all states from a database connecting to an sql server 
+    and now only listing ones starting with N """
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        db=sys.argv[3]
+    )
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
