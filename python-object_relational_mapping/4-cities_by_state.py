fb0e596c6ec0a14fcf5a226f6f57be9d88226173
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-06-25 03:57:19 +0000

    task 4 cities by state

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Getting some cities in here, city boy city boy """
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute(
+                   "SELECT cities.id, cities.name, states.name "
+                   "FROM cities "
+                   "INNER JOIN states "
+                   "ON cities.state_id = states.id "
+                   "ORDER BY cities.id ASC",
+    )
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
