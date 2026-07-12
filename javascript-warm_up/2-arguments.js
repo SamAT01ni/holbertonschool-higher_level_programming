4dab11e0af75b0e1b184e3898163189ae02d3e0e
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-07-12 13:26:54 +1000

    task 2

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// you seem argumentative today
+
+const argc = process.argv.length - 2;
+
+if (argc === 0) {
+    console.log('No argument');
+}   else if (argc === 1) {
+    console.log('Argument found');
+}   else {
+    console.log('Arguments found');
+}
