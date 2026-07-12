99792a77d4af9fdcf429a9bdf9fca1af8df4d089
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-07-12 13:06:41 +1000

    capital s

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // printing in other languages. Como ce dice my balls itch
 
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
