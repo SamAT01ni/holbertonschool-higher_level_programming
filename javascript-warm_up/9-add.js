60fa0aba43d54b21bc818a13d157d586502e9ff8
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-07-12 14:44:45 +1000

    9

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// addition and stuff
+
+const argv = process.argv.slice(2);
+const a = Number(argv[0]);
+const b = Number(argv[1]);
+
+function add (a, b) {
+    return a + b;
+}
+
+console.log(add(a, b));
