6a0087f945e238cf5d3814c1b093132a4dfb4c00
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-07-13 15:59:12 +1000

    3

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+document.getElementById('toggle_header').addEventListener('click', () => {
+    document.querySelector('header').classList.toggle('red');
+    document.querySelector('header').classList.toggle('green');
+});
